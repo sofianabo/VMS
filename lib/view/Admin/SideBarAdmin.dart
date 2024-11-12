072f979b7870da91c88ba80056b780a78402a777
Laith Azzam <Laetheazzam.la.la@gmail.com>
2024-11-12 14:25:49 +0200

    Fix Widget commit

--- lib/view/Admin/SideBarAdmin.dart
+++ lib/view/Admin/SideBarAdmin.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx/Link/Controller/AdminController/AdminHomeContentController.dart';
--- lib/view/Admin/TeacherManagementGrid.dart
+++ lib/view/Admin/TeacherManagementGrid.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx/Icons_File/v_m_s__icons_icons.dart';
