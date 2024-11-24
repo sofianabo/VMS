5321784d5be3edd060aa37bfcaeff979730e3bd8
sofian abo shdeed <sofian726d@gmail.com>
2024-11-24 12:46:21 +0300

    nothing

--- lib/Link/Controller/AdminController/DropDownClassesController.dart
+++ lib/Link/Controller/AdminController/DropDownClassesController.dart
@@ -1,16 +1,9 @@
 import 'package:get/get.dart';
-import 'package:getx/Link/Controller/AdminController/AllStudentsController.dart';
-import 'package:getx/Link/Controller/AdminController/DropDownDivisionController.dart';
 import 'package:getx/Link/Model/AdminModel/AllClassesModel.dart';
-import 'package:getx/Link/Model/AdminModel/AllDivisionModel.dart';
-import 'package:getx/Link/Model/AdminModel/AllGradeModel.dart';
-import 'package:getx/Link/Model/AdminModel/AllSessionModel.dart';
 
 class Dropdownclassescontroller extends GetxController {
   List<String> classes = [];
   List<Class> Allclass = [];
-  AllDivisionModel? division;
-  Allstudentscontroller studentcontroller = Get.find<Allstudentscontroller>();
   void setClasses(AllClassesModel clas) {
     Allclass = clas.classes;
     classes.clear();
@@ -19,6 +12,4 @@ class Dropdownclassescontroller extends GetxController {
     }
     update();
   }
-
-
 }
