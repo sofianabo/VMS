454eebe3371c3808c6b209a52da8a5f986979dbb
sofian abo shdeed <sofian726d@gmail.com>
2024-11-26 09:50:21 +0300

    api

--- lib/Link/API/API.dart
+++ lib/Link/API/API.dart
@@ -1,6 +1,6 @@
 //  http://192.168.43.127:8000/api/
 //https://5e24-149-102-244-69.ngrok-free.app/api/
-String hostPort = "http://192.168.217.56:8000/api/";
+String hostPort = "http://192.168.43.127:8000/api/";
 
 //home
 String Home = "homeScreen";
