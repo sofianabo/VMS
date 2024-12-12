9b3ed717714ebffa550931a4c7d4f45c0065f95f
sofian abo shdeed <sofian726d@gmail.com>
2024-12-12 11:01:09 +0300

    null

--- lib/Link/API/AdminAPI/Students_APIs/RequestsAPI.dart
+++ lib/Link/API/AdminAPI/Students_APIs/RequestsAPI.dart
@@ -1,12 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:vms_school/Link/API/API.dart';
 import 'package:vms_school/Link/API/AdminAPI/School/School_DropDown/DropdownClassesAPI.dart';
 import 'package:vms_school/Link/API/Error_API.dart';
 import 'package:vms_school/Link/Controller/AdminController/Students_Controllers/RequestsController.dart';
 import 'package:vms_school/Link/Model/AdminModel/AllClassesModel.dart';
 import 'package:vms_school/Link/Model/AdminModel/RequestsModel.dart';
-import '../../API.dart' as global;
 import 'package:vms_school/Link/API/DioOption.dart';
 
 class GetAllRequestsapi {
@@ -22,8 +22,9 @@ class GetAllRequestsapi {
       c.setAllClassDialog(classesModel);
       c.restor();
 
-      String myurl = "${global.hostPort}${global.getRequests}";
-      var response = await dio.get(myurl, data: {}, options: getDioOptions());
+      String myurl = "${hostPort}${getRequests}";
+      var response = await dio.get(myurl, options: getDioOptions());
+
       if (response.statusCode == 200) {
         AllRequestsModel requests = AllRequestsModel.fromJson(response.data);
         c.setAllRequests(requests);
