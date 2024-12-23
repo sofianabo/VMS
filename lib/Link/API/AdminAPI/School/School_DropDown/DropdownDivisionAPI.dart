985d5d9e6f914db2dd7fbfef70eaefbf9515a67e
sofian abo shdeed <sofian726d@gmail.com>
2024-12-23 10:25:01 +0300

    drop

--- lib/Link/API/AdminAPI/School/School_DropDown/DropdownDivisionAPI.dart
+++ lib/Link/API/AdminAPI/School/School_DropDown/DropdownDivisionAPI.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:vms_school/Link/API/API.dart';
 import 'package:vms_school/Link/API/Error_API.dart';
+import 'package:vms_school/Link/Controller/AdminController/Students_Controllers/AllStudentsController.dart';
 import 'package:vms_school/Link/Controller/WidgetController/DropDown_Controllers/DropDownClassesController.dart';
 import 'package:vms_school/Link/Controller/WidgetController/DropDown_Controllers/DropDownDivisionController.dart';
 import 'package:vms_school/Link/Model/AdminModel/AllDivisionModel.dart';
@@ -10,6 +11,7 @@ import 'package:vms_school/Link/API/DioOption.dart';
 
 class Dropdowndivisionapi {
   Dropdowndivisioncontroller c = Get.find<Dropdowndivisioncontroller>();
+  final Allstudentscontroller stuc = Get.find<Allstudentscontroller>();
 
   Dropdownclassescontroller class_controller =
       Get.find<Dropdownclassescontroller>();
@@ -19,6 +21,8 @@ class Dropdowndivisionapi {
 
   Dropdowndivision(int idx) async {
     try {
+      c.setIsLoading(true);
+      stuc.setDivisionLoading(true);
       int? id = class_controller.Allclass[idx].id;
       String myurl = "${hostPort}${getDivision}";
       var response = await dio.post(myurl,
@@ -27,7 +31,7 @@ options: getDioOptions());
       if (response.statusCode == 200) {
         AllDivisionModel division = AllDivisionModel.fromJson(response.data);
         c.setDivision(division);
-        return division;
+
       } else {
         ErrorHandler.handleDioError(DioError(
           requestOptions: response.requestOptions,
