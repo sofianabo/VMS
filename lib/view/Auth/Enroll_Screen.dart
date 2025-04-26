fdc79b72877d3af76956e0f173704b85ecb1a47e
laeth <laetheazzam.la.la@gmail.com>
2025-04-26 13:27:01 +0300

    fix Commit

---
+++ lib/view/Auth/Enroll_Screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EnrollScreen extends StatefulWidget {
+  const EnrollScreen({super.key});
+
+  @override
+  State<EnrollScreen> createState() => _EnrollScreenState();
+}
+
+class _EnrollScreenState extends State<EnrollScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
